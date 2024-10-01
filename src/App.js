43d8f68befab75f6ae43172cc676ee5f9a59f3b7
leonardoasantos1 <leonardoasantos1@outlook.com>
2024-09-30 23:07:08 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React JS'
+  ]);
+
+  function handleRegister(e){
+    e.preventDefault();
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando usuario</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa:</label><br/>
+        <input 
+          placeholder="Digite uma tarefa" 
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
+        /><br/>
+        <button type="submit">Registrar</button>
+      </form>
+      <br/><br/>
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
